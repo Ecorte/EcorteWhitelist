fac69b9205000ac753d11ba486dc6d21fba94559
Ecorte <me@ecorte.xyz>
2025-12-14 16:28:45 -0500

    1.2

--- src/main/java/org/ecorte/ecorteWhitelist/EcorteWhitelist.java
+++ src/main/java/org/ecorte/ecorteWhitelist/EcorteWhitelist.java
@@ -1,14 +1,17 @@
 package org.ecorte.ecorteWhitelist;
 
+import com.destroystokyo.paper.profile.PlayerProfile;
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPIConfig;
+import dev.jorel.commandapi.CommandAPIPaperConfig;
+import dev.jorel.commandapi.arguments.AsyncPlayerProfileArgument;
 import dev.jorel.commandapi.arguments.BooleanArgument;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bstats.bukkit.Metrics;
 import dev.jorel.commandapi.CommandAPICommand;
-import dev.jorel.commandapi.arguments.OfflinePlayerArgument;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.context.ContextManager;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.sql.Connection;
@@ -17,8 +20,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public final class EcorteWhitelist extends JavaPlugin {
     public Config config;
@@ -27,65 +32,108 @@ public final class EcorteWhitelist extends JavaPlugin {
 
     @Override
     public void onLoad() {
+        CommandAPI.onLoad(new CommandAPIPaperConfig(this)
+                .verboseOutput(true)
+                .silentLogs(true));
+
+        this.registerCommands();
+    }
+
+    public void registerCommands() {
         // Plugin startup logic
         CommandAPICommand AddCommand = new CommandAPICommand("add")
                 .withPermission("ecortewhitelist.whitelist.add")
-                .withArguments(new OfflinePlayerArgument("player"))
+                .withArguments(new AsyncPlayerProfileArgument("player"))
                 .executes((sender, args) -> {
-                    OfflinePlayer player = (OfflinePlayer) args.get("player");
-                    var mm = MiniMessage.miniMessage();
-                    Component parsed;
-                    if (player == null) {
-                        parsed = mm.deserialize(this.config.responsePrefix + " Player not found");
+                    CompletableFuture<List<PlayerProfile>> profiles = (CompletableFuture<List<PlayerProfile>>) args.get("player");
+
+                    assert profiles != null;
+                    profiles.thenAccept(profileList -> {
+                        var mm = MiniMessage.miniMessage();
+                        Component parsed;
+                        if (profileList == null || profileList.isEmpty()) {
+                            parsed = mm.deserialize(this.config.responsePrefix + " Player not found");
+                            sender.sendMessage(parsed);
+                            return;
+                        }
+                        PlayerProfile profile = profileList.get(0);
+                        UUID uuid = profile.getUniqueId();
+                        UUID senderUUID = UUID.fromString("00000000-0000-0000-0000-000000000000");
+                        if (sender instanceof Player) senderUUID = ((Player) sender).getUniqueId();
+                        this.AddToWhitelist(uuid, senderUUID);
+                        this.getComponentLogger().info(Component.text("Player " + profile.getName() + " added to the whitelist by " + sender.getName()));
+                        parsed = mm.deserialize(this.config.responsePrefix + " Player <green>" + profile.getName() + "</green> added to the whitelist by <gold>" + sender.getName() + "</gold>");
                         sender.sendMessage(parsed);
-                        return;
-                    }
-                    UUID uuid = player.getUniqueId();
-                    UUID senderUUID = UUID.fromString("00000000-0000-0000-0000-000000000000");
-                    if (sender instanceof Player) senderUUID = ((Player) sender).getUniqueId();
-                    this.AddToWhitelist(uuid, senderUUID);
-                    this.getComponentLogger().info(Component.text("Player " + player.getName() + " added to the whitelist by " + sender.getName()));
-                    parsed = mm.deserialize(this.config.responsePrefix + " Player <green>" + player.getName() + "</green> added to the whitelist by <gold>" + sender.getName() + "</gold>");
-                    sender.sendMessage(parsed);
+                    }).exceptionally(throwable -> {
+                        Throwable cause = throwable.getCause();
+                        Throwable rootCause = cause instanceof RuntimeException ? cause.getCause() : cause;
+
+                        sender.sendMessage("An error occurred while fetching the player profile: " + rootCause.getMessage());
+                        return null;
+                    });
                 });
 
         CommandAPICommand RemoveCommand = new CommandAPICommand("remove")
                 .withPermission("ecortewhitelist.whitelist.remove")
-                .withArguments(new OfflinePlayerArgument("player"))
+                .withArguments(new AsyncPlayerProfileArgument("player"))
                 .executes((sender, args) -> {
-                    OfflinePlayer player = (OfflinePlayer) args.get("player");
-                    var mm = MiniMessage.miniMessage();
-                    Component parsed;
-                    if (player == null) {
-                        parsed = mm.deserialize(this.config.responsePrefix + " Player not found");
+                    CompletableFuture<List<PlayerProfile>> profiles = (CompletableFuture<List<PlayerProfile>>) args.get("player");
+
+                    assert profiles != null;
+                    profiles.thenAccept(profileList -> {
+                        var mm = MiniMessage.miniMessage();
+                        Component parsed;
+                        if (profileList == null || profileList.isEmpty()) {
+                            parsed = mm.deserialize(this.config.responsePrefix + " Player not found");
+                            sender.sendMessage(parsed);
+                            return;
+                        }
+                        PlayerProfile profile = profileList.get(0);
+                        UUID uuid = profile.getUniqueId();
+                        this.RemoveFromWhitelist(uuid);
+                        parsed = mm.deserialize(this.config.responsePrefix + " Player <green>" + profile.getName() + "</green> removed from the whitelist");
                         sender.sendMessage(parsed);
-                        return;
-                    }
-                    UUID uuid = player.getUniqueId();
-                    this.RemoveFromWhitelist(uuid);
-                    parsed = mm.deserialize(this.config.responsePrefix + " Player <green>" + player.getName() + "</green> removed from the whitelist");
-                    sender.sendMessage(parsed);
+                    }).exceptionally(throwable -> {
+                        Throwable cause = throwable.getCause();
+                        Throwable rootCause = cause instanceof RuntimeException ? cause.getCause() : cause;
+
+                        sender.sendMessage("An error occurred while fetching the player profile: " + rootCause.getMessage());
+                        return null;
+                    });
                 });
 
         CommandAPICommand StatusCommand = new CommandAPICommand("status")
                 .withPermission("ecortewhitelist.whitelist.status")
-                .withArguments(new OfflinePlayerArgument("player"))
+                .withArguments(new AsyncPlayerProfileArgument("player"))
                 .executes((sender, args) -> {
-                    OfflinePlayer player = (OfflinePlayer) args.get("player");
-                    var mm = MiniMessage.miniMessage();
-                    Component parsed;
-                    if (player == null) {
-                        parsed = mm.deserialize(this.config.responsePrefix + " Player not found");
+
+                    CompletableFuture<List<PlayerProfile>> profiles = (CompletableFuture<List<PlayerProfile>>) args.get("player");
+
+                    assert profiles != null;
+                    profiles.thenAccept(profileList -> {
+
+                        var mm = MiniMessage.miniMessage();
+                        Component parsed;
+                        if (profileList == null || profileList.isEmpty()) {
+                            parsed = mm.deserialize(this.config.responsePrefix + " Player not found");
+                            sender.sendMessage(parsed);
+                            return;
+                        }
+                        PlayerProfile profile = profileList.get(0);
+                        UUID uuid = profile.getUniqueId();
+                        if (this.whitelist.contains(uuid)) {
+                            parsed = mm.deserialize(this.config.responsePrefix + " Player <green>" + profile.getName() + "</green> is whitelisted");
+                        } else {
+                            parsed = mm.deserialize(this.config.responsePrefix + " Player <green>" + profile.getName() + "</green> is not whitelisted");
+                        }
                         sender.sendMessage(parsed);
-                        return;
-                    }
-                    UUID uuid = player.getUniqueId();
-                    if (this.whitelist.contains(uuid)) {
-                        parsed = mm.deserialize(this.config.responsePrefix + " Player <green>" + player.getName() + "</green> is whitelisted");
-                    } else {
-                        parsed = mm.deserialize(this.config.responsePrefix + " Player <green>" + player.getName() + "</green> is not whitelisted");
-                    }
-                    sender.sendMessage(parsed);
+                    }).exceptionally(throwable -> {
+                        Throwable cause = throwable.getCause();
+                        Throwable rootCause = cause instanceof RuntimeException ? cause.getCause() : cause;
+
+                        sender.sendMessage("An error occurred while fetching the player profile: " + rootCause.getMessage());
+                        return null;
+                    });
                 });
 
         CommandAPICommand ToggleCommand = new CommandAPICommand("toggle")
@@ -190,6 +238,8 @@ public final class EcorteWhitelist extends JavaPlugin {
         int pluginId = 24211;
         Metrics metrics = new Metrics(this, pluginId);
 
+        CommandAPI.onEnable();
+
         // Plugin startup logic
         this.config = Config.getInstance(this);
         this.dbManager = DBManager.getInstance(this);
@@ -209,6 +259,7 @@ public final class EcorteWhitelist extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        CommandAPI.onDisable();
         // Plugin shutdown logic
         this.dbManager.close();
     }
